3ba23a1de4967ca29aeb6632f768981910c47966
XieBaoshi <69102900+XieBaoshi@users.noreply.github.com>
2021-04-25 19:44:47 +0200

    Update training_loop.py

--- training/training_loop.py
+++ training/training_loop.py
@@ -99,7 +99,7 @@ def training_loop(
     random_seed             = 0,        # Global random seed.
     num_gpus                = 1,        # Number of GPUs participating in the training.
     rank                    = 0,        # Rank of the current process in [0, num_gpus[.
-    batch_size              = 68,        # Total batch size for one training iteration. Can be larger than batch_gpu * num_gpus.
+    batch_size              = 32,        # Total batch size for one training iteration. Can be larger than batch_gpu * num_gpus.
     batch_gpu               = 4,        # Number of samples processed at a time by one GPU.
     ema_kimg                = 10,       # Half-life of the exponential moving average (EMA) of generator weights.
     ema_rampup              = None,     # EMA ramp-up coefficient.
@@ -111,9 +111,9 @@ def training_loop(
     ada_kimg                = 500,      # ADA adjustment speed, measured in how many kimg it takes for p to increase/decrease by one unit.
     total_kimg              = 25000,    # Total length of the training, measured in thousands of real images.
     kimg_per_tick           = 4,        # Progress snapshot interval.
-    image_snapshot_ticks    = 50,       # How often to save image snapshots? None = disable.
-    network_snapshot_ticks  = 50,       # How often to save network snapshots? None = disable.
-    resume_pkl              = './content/drive/MyDrive/output/00002-ico-mirror-auto1-resumecustom/network-snapshot-000060.pkl',     # Network pickle to resume training from.
+    image_snapshot_ticks    = 5       # How often to save image snapshots? None = disable.
+    network_snapshot_ticks  = 30,       # How often to save network snapshots? None = disable.
+    resume_pkl              = '',     # Network pickle to resume training from.
     cudnn_benchmark         = True,     # Enable torch.backends.cudnn.benchmark?
     allow_tf32              = False,    # Enable torch.backends.cuda.matmul.allow_tf32 and torch.backends.cudnn.allow_tf32?
     abort_fn                = None,     # Callback function for determining whether to abort training. Must return consistent results across ranks.
